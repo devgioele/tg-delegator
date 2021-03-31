d3e57a3f23c808906e6676ccc91529663559ce6e
devgioele <63794725+devgioele@users.noreply.github.com>
2021-03-31 10:07:30 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import YAML from 'yaml';
+import TeleBot from 'telebot';
+
+// Load config
+console.log('Loading config')
+const file = fs.readFileSync('./config.yaml', 'utf8');
+const config = YAML.parse(file);
+console.log(`Loaded ${safeLength(config.masters)} masters and`,
+  `${safeLength(config.receivers)} receivers`)
+const bot = new TeleBot(config.token);
+
+// Event handlers
+const relay = (msg) => {
+  console.log('Received from' + ' @' + msg.from.username, 'with id', msg.from.id);
+
+  if (config.masters.empty() || config.masters.includes(msg.from.id)) {
+    console.log('Replying');
+
+    return config.receivers.map(id => bot.sendMessage(id, msg.text)
+      .catch((error) => {
+        console.log('Error: ', error);
+      }));
+  }
+}
+
+// Start bot
+bot.on('text', relay);
+bot.start();
+
+function safeLength(arr) {
+  if(arr === undefined || arr === null)
+    return 0;
+  return arr.length;
+}
